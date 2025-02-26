f4cfcc6207a562fddc29a2e11b86b75eeb6637dc
Simon Rothman <srothman@wesleyan.edu>
2025-02-26 10:46:12 -0500

    Seprated Res3 backend into its own header

---
+++ Res3Backend.h
@@ -0,0 +1,355 @@
+#include "Adjacency.h"
+
+#include "SRothman/SimonTools/src/deltaR.h"
+#include "SRothman/SimonTools/src/histutil.h"
+#include "SRothman/SimonTools/src/util.h"
+
+#include <memory>
+
+#include <array>
+
+template <bool distances_squared>
+inline void get_indices_CA(const double dR12, const double dR13, const double dR23,
+                           const double E1, const double eta1, const double phi1,
+                           const double E2, const double eta2, const double phi2,
+                           const double E3, const double eta3, const double phi3,
+                           const double deta12, const double dphi12,
+                           const double deta13, const double dphi13,
+                           const double deta23, const double dphi23,
+                           unsigned& R_idx, unsigned& r_idx, unsigned& c_idx,
+                           const EEC::Res3Axes& axes) noexcept {
+
+    std::array<std::pair<double, unsigned>, 3> dRs = {
+        {std::make_pair(dR12, 0), std::make_pair(dR13, 1), std::make_pair(dR23, 2)}
+    };
+    if constexpr(distances_squared){
+        for(auto& dR : dRs){
+            dR.first = std::sqrt(dR.first);
+        }
+    }
+    std::sort(dRs.begin(), dRs.end());
+
+    if (dRs[0].first == 0){
+        R_idx = 0;
+        r_idx = 0;
+        c_idx = 0;
+    } else if (dRs[2].first == 0){
+        R_idx = simon::getIndex(dRs[0].first, axes.R);
+        r_idx = 0;
+        c_idx = 0;
+    } else {
+        double etaFar, phiFar;
+        double etaMid, phiMid;
+        double detaSmall, dphiSmall;
+        if (dRs[2].second == 0){//shortest leg is dR12
+            etaFar = eta3;
+            phiFar = phi3;
+
+            double Esum = E1 + E2;
+            etaMid = (E1*eta1 + E2*eta2) / Esum;
+            phiMid = (E1*phi1 + E2*phi2) / Esum;
+
+            detaSmall = deta12;
+            dphiSmall = dphi12;
+        } else if(dRs[2].second == 1){//shortest leg is dR13
+            etaFar = eta2;
+            phiFar = phi2;
+
+            double Esum = E1 + E3;
+            etaMid = (E1*eta1 + E3*eta3) / Esum;
+            phiMid = (E1*phi1 + E3*phi3) / Esum;
+
+            detaSmall = deta13;
+            dphiSmall = dphi13;
+        } else {//shortest leg is dR23
+            etaFar = eta1;
+            phiFar = phi1;
+
+            double Esum = E2 + E3;
+            etaMid = (E2*eta2 + E3*eta3) / Esum;
+            phiMid = (E2*phi2 + E3*phi3) / Esum;
+
+            detaSmall = deta23;
+            dphiSmall = dphi23;
+        }
+
+        double detaLong = simon::deltaEta(etaFar, etaMid);
+        double dphiLong = simon::deltaPhi(phiFar, phiMid);
+
+        double R = std::sqrt(simon::square(detaLong) + simon::square(dphiLong));
+        double r = dRs[2].first / R;
+
+        double dot = (detaLong*detaSmall + dphiLong*dphiSmall) / (R * dRs[2].first);
+        if (dot > 1){
+            dot = 1;
+        } else if (dot < -1){
+            dot = -1;
+        }
+        double c = std::acos(dot);
+        if (c > M_PI/2){
+            c = M_PI - c;
+        }
+
+        R_idx = simon::getIndex(R, axes.R);
+        r_idx = simon::getIndex(r, axes.r);
+        c_idx = simon::getIndex(c, axes.c);
+    }
+}
+
+template <bool distances_squared>
+inline void get_indices(const double dR12, const double dR13, const double dR23,
+                        unsigned& R_idx, unsigned& r_idx, unsigned& c_idx,
+                        const EEC::Res3Axes& axes) noexcept {
+
+    std::array<double, 3> dRs = {dR12, dR13, dR23};
+    if constexpr(distances_squared){
+        for(auto& dR : dRs){
+            dR = std::sqrt(dR);
+        }
+    }
+    std::sort(dRs.begin(), dRs.end());
+    if (dRs[0] == 0){
+        R_idx = 0;
+        r_idx = 0;
+        c_idx = 0;
+    } else if (dRs[2] == 0){
+        R_idx = simon::getIndex(dRs[0], axes.R);
+        r_idx = 0;
+        c_idx = 0;
+    } else {
+        double R = dRs[0];
+        double r = dRs[2]/dRs[1];
+        double c = std::asin(std::sqrt(1 - simon::square((dRs[0] - dRs[1])/dRs[2])));
+        R_idx = simon::getIndex(R, axes.R);
+        r_idx = simon::getIndex(r, axes.r);
+        c_idx = simon::getIndex(c, axes.c);
+    }
+}
+
+template <bool isCA, class PairsType, class TransferContainer>
+inline void res3_transferloop(
+        EEC::Res3TransferResult<TransferContainer>& transfer,
+
+        const std::shared_ptr<const EEC::EECjet<PairsType>> thisjet_reco,
+
+        const EEC::Res3Axes& axes_reco,
+
+        const unsigned R_idx_gen,
+        const unsigned r_idx_gen,
+        const unsigned c_idx_gen,
+
+        const EEC::neighborhood& n1,
+        const EEC::neighborhood& n2,
+        const EEC::neighborhood& n3,
+
+        const double wt_gen) noexcept {
+
+    for(const EEC::neighbor& j1 : n1){
+        [[maybe_unused]] auto&[E1, eta1, phi1] = thisjet_reco->singles.get(j1.idx);
+        const double twt1 = wt_gen * j1.wt;
+
+        for(const EEC::neighbor& j2 : n2){
+            [[maybe_unused]] auto&[E2, eta2, phi2] = thisjet_reco->singles.get(j2.idx);
+            const double twt2 = twt1 * j2.wt;
+
+            const auto&[deta12, dphi12, dR12] = thisjet_reco->pairs.get(j1.idx, j2.idx);
+
+            for(const EEC::neighbor& j3 : n3){
+                [[maybe_unused]] auto&[E3, eta3, phi3] = thisjet_reco->singles.get(j3.idx);
+                const double twt3 = twt2 * j3.wt;
+
+                const auto&[deta13, dphi13, dR13] = thisjet_reco->pairs.get(j1.idx, j3.idx);
+                const auto&[deta23, dphi23, dR23] = thisjet_reco->pairs.get(j2.idx, j3.idx);
+
+                unsigned R_idx_reco, r_idx_reco, c_idx_reco;
+                if constexpr (isCA){
+                    get_indices_CA<PairsType::distances_squared>(
+                        dR12, dR13, dR23,
+                        E1, eta1, phi1,
+                        E2, eta2, phi2,
+                        E3, eta3, phi3,
+                        deta12, dphi12,
+                        deta13, dphi13,
+                        deta23, dphi23,
+                        R_idx_reco, r_idx_reco, c_idx_reco,
+                        axes_reco);
+                } else {
+                    get_indices<PairsType::distances_squared>(dR12, dR13, dR23, R_idx_reco, r_idx_reco, c_idx_reco, axes_reco);
+                }
+
+                transfer.fill(R_idx_reco, r_idx_reco, c_idx_reco,
+                              R_idx_gen, r_idx_gen, c_idx_gen, 
+                              twt3, wt_gen);
+            }
+        }
+    }
+}
+
+template <bool isCA, class BasicContainer, class PairsType, bool doUnmatched, class TransferContainer, bool doTransfer>
+inline void res3_mainloop(
+        EEC::Res3Result<BasicContainer>& result,
+        [[maybe_unused]] EEC::Res3Result<BasicContainer>* unmatched_gen,
+        [[maybe_unused]] EEC::Res3TransferResult<TransferContainer>* transfer,
+
+        [[maybe_unused]] const std::shared_ptr<const EEC::EECjet<PairsType>> thisjet_reco,
+        const std::shared_ptr<const EEC::EECjet<PairsType>> thisjet_gen,
+        [[maybe_unused]] const std::vector<bool>* matched,
+
+        [[maybe_unused]] const std::shared_ptr<const EEC::Adjacency> adj,
+
+        [[maybe_unused]] const EEC::Res3Axes* axes_reco,
+        const EEC::Res3Axes& axes_gen) noexcept {//TODO: REMOVE [[maybe_unused]] here
+
+    for(unsigned i1=0; i1<thisjet_gen->N; ++i1){
+        const auto&[E1, eta1, phi1] = thisjet_gen->singles.get(i1);
+        [[maybe_unused]] bool matched1;
+        if constexpr(doUnmatched){
+            matched1 = matched->at(i1);
+        }
+        EEC::neighborhood const * n1 = nullptr;
+        if constexpr(doTransfer){
+            n1 = &(adj->get_neighborhood(i1));
+        }
+
+        /*
+         * One-particle part
+         * This has symmetry factor 1
+         * And coordinates (R, r, c) = (0, 0, 0)
+         */
+        double wt = E1*E1*E1;
+        result.fill(0, 0, 0, wt);
+        if constexpr(doUnmatched){
+            if(!matched1){
+                unmatched_gen->fill(0, 0, 0, wt);
+            }
+        }
+        if constexpr(doTransfer){
+            res3_transferloop<isCA>(
+                    *transfer,
+                    thisjet_reco,
+                    *axes_reco,
+                    0, 0, 0,
+                    *n1,
+                    *n1,
+                    *n1,
+                    wt);
+        }
+
+
+        for(unsigned i2=i1+1; i2<thisjet_gen->N; ++i2){
+            const auto&[E2, eta2, phi2] = thisjet_gen->singles.get(i2);
+            [[maybe_unused]] bool matched2;
+            if constexpr(doUnmatched){
+                matched2 = matched1 && matched->at(i2);
+            }
+            EEC::neighborhood const * n2 = nullptr;
+            if constexpr(doTransfer){
+                n2 = &(adj->get_neighborhood(i2));
+            }
+
+            const double E12 = E1*E2;
+
+            const auto&[deta12, dphi12, dR12] = thisjet_gen->pairs.get(i1, i2);
+
+            /*
+             * Two-particle part
+             * This has symmetry factor 3!/2! = 3
+             * And RL = R12, r=c=0
+             */
+            unsigned R_idx;
+            if constexpr(PairsType::distances_squared){
+                R_idx = simon::getIndex(std::sqrt(dR12), axes_gen.R);
+            } else {
+                R_idx = simon::getIndex(dR12, axes_gen.R);
+            }
+            wt = 3 * E12 * (E1 + E2);
+            result.fill(R_idx, 0, 0, wt);
+            if constexpr(doUnmatched){
+                if(!matched2){
+                    unmatched_gen->fill(R_idx, 0, 0, wt);
+                }
+            }
+            if constexpr(doTransfer){
+                double wt1 = 3*E12*E1;
+                res3_transferloop<isCA>(
+                        *transfer,
+                        thisjet_reco,
+                        *axes_reco,
+                        R_idx, 0, 0,
+                        *n1,
+                        *n1,
+                        *n2,
+                        wt1);
+                double wt2 = 3*E12*E2;
+                res3_transferloop<isCA>(
+                        *transfer,
+                        thisjet_reco,
+                        *axes_reco,
+                        R_idx, 0, 0,
+                        *n1,
+                        *n2,
+                        *n2,
+                        wt2);
+            }
+
+            for(unsigned i3=i2+1; i3<thisjet_gen->N; ++i3){
+                const auto&[E3, eta3, phi3] = thisjet_gen->singles.get(i3);
+                [[maybe_unused]] bool matched3;
+                if constexpr(doUnmatched){
+                    matched3 = matched2 && matched->at(i3);
+                }
+                EEC::neighborhood const * n3 = nullptr;
+                if constexpr(doTransfer){
+                    n3 = &(adj->get_neighborhood(i3));
+                }
+
+                const double E123 = E12*E3;
+
+                const auto&[deta13, dphi13, dR13] = thisjet_gen->pairs.get(i1, i3);
+                const auto&[deta23, dphi23, dR23] = thisjet_gen->pairs.get(i2, i3);
+
+                /*
+                 * Three-particle part
+                 * This has symmetry factor 3! = 6
+                 * and full coordinates
+                 */
+                unsigned r_idx, c_idx;
+                if constexpr (isCA){
+                    get_indices_CA<PairsType::distances_squared>(
+                        dR12, dR13, dR23,
+                        E1, eta1, phi1,
+                        E2, eta2, phi2,
+                        E3, eta3, phi3,
+                        deta12, dphi12,
+                        deta13, dphi13,
+                        deta23, dphi23,
+                        R_idx, r_idx, c_idx,
+                        axes_gen);
+                } else {
+                    get_indices<PairsType::distances_squared>(dR12, dR13, dR23, R_idx, r_idx, c_idx, axes_gen);
+                }
+                
+                wt = E123*6;
+                result.fill(R_idx, r_idx, c_idx, wt);
+                if constexpr(doUnmatched){
+                    if(!matched3){
+                        unmatched_gen->fill(R_idx, r_idx, c_idx, wt);
+                    }
+                }
+                if constexpr(doTransfer){
+                    res3_transferloop<isCA>(
+                            *transfer,
+                            thisjet_reco,
+                            *axes_reco,
+                            R_idx, r_idx, c_idx,
+                            *n1,
+                            *n2,
+                            *n3,
+                            wt);
+                }
+            }
+        }
+    }
+}
+
+
--- Res3Calculator.cc
+++ Res3Calculator.cc
@@ -2,6 +2,7 @@
 #include "Adjacency.h"
 #include "Res3Result.h"
 #include "Res3TransferResult.h"
+#include "Res3Backend.h"
 
 #include "SRothman/SimonTools/src/deltaR.h"
 #include "SRothman/SimonTools/src/histutil.h"
@@ -10,233 +11,6 @@
 #include <memory>
 
 #include <array>
-
-template <bool distances_squared>
-static void get_indices(const double dR12, const double dR13, const double dR23,
-                        unsigned& R_idx, unsigned& r_idx, unsigned& c_idx,
-                        const EEC::Res3Axes& axes) noexcept {
-
-    std::array<double, 3> dRs = {dR12, dR13, dR23};
-    if constexpr(distances_squared){
-        for(auto& dR : dRs){
-            dR = std::sqrt(dR);
-        }
-    }
-    std::sort(dRs.begin(), dRs.end());
-    if (dRs[0] == 0){
-        R_idx = 0;
-        r_idx = 0;
-        c_idx = 0;
-    } else if (dRs[2] == 0){
-        R_idx = simon::getIndex(dRs[0], axes.R);
-        r_idx = 0;
-        c_idx = 0;
-    } else {
-        double R = dRs[0];
-        double r = dRs[2]/dRs[1];
-        double c = std::asin(std::sqrt(1 - simon::square((dRs[0] - dRs[1])/dRs[2])));
-        R_idx = simon::getIndex(R, axes.R);
-        r_idx = simon::getIndex(r, axes.r);
-        c_idx = simon::getIndex(c, axes.c);
-    }
-}
-
-template <class PairsType, class TransferContainer>
-static void res3_transferloop(
-        EEC::Res3TransferResult<TransferContainer>& transfer,
-
-        const std::shared_ptr<const EEC::EECjet<PairsType>> thisjet_reco,
-
-        const EEC::Res3Axes& axes_reco,
-
-        const unsigned R_idx_gen,
-        const unsigned r_idx_gen,
-        const unsigned c_idx_gen,
-
-        const EEC::neighborhood& n1,
-        const EEC::neighborhood& n2,
-        const EEC::neighborhood& n3,
-
-        const double wt_gen) noexcept {
-
-    for(const EEC::neighbor& j1 : n1){
-        const double twt1 = wt_gen * j1.wt;
-
-        for(const EEC::neighbor& j2 : n2){
-            const double twt2 = twt1 * j2.wt;
-
-            const auto&[deta12, dphi12, dR12] = thisjet_reco->pairs.get(j1.idx, j2.idx);
-
-            for(const EEC::neighbor& j3 : n3){
-                const double twt3 = twt2 * j3.wt;
-
-                const auto&[deta13, dphi13, dR13] = thisjet_reco->pairs.get(j1.idx, j3.idx);
-                const auto&[deta23, dphi23, dR23] = thisjet_reco->pairs.get(j2.idx, j3.idx);
-
-                unsigned R_idx_reco, r_idx_reco, c_idx_reco;
-                get_indices<PairsType::distances_squared>(dR12, dR13, dR23, R_idx_reco, r_idx_reco, c_idx_reco, axes_reco);
-
-                transfer.fill(R_idx_reco, r_idx_reco, c_idx_reco,
-                              R_idx_gen, r_idx_gen, c_idx_gen, 
-                              twt3, wt_gen);
-            }
-        }
-    }
-}
-
-template <class BasicContainer, class PairsType, bool doUnmatched, class TransferContainer, bool doTransfer>
-static void res3_mainloop(
-        EEC::Res3Result<BasicContainer>& result,
-        [[maybe_unused]] EEC::Res3Result<BasicContainer>* unmatched_gen,
-        [[maybe_unused]] EEC::Res3TransferResult<TransferContainer>* transfer,
-
-        [[maybe_unused]] const std::shared_ptr<const EEC::EECjet<PairsType>> thisjet_reco,
-        const std::shared_ptr<const EEC::EECjet<PairsType>> thisjet_gen,
-        [[maybe_unused]] const std::vector<bool>* matched,
-
-        [[maybe_unused]] const std::shared_ptr<const EEC::Adjacency> adj,
-
-        [[maybe_unused]] const EEC::Res3Axes* axes_reco,
-        const EEC::Res3Axes& axes_gen) noexcept {//TODO: REMOVE [[maybe_unused]] here
-
-    for(unsigned i1=0; i1<thisjet_gen->N; ++i1){
-        const auto&[E1, eta1, phi1] = thisjet_gen->singles.get(i1);
-        [[maybe_unused]] bool matched1;
-        if constexpr(doUnmatched){
-            matched1 = matched->at(i1);
-        }
-        EEC::neighborhood const * n1 = nullptr;
-        if constexpr(doTransfer){
-            n1 = &(adj->get_neighborhood(i1));
-        }
-
-        /*
-         * One-particle part
-         * This has symmetry factor 1
-         * And coordinates (R, r, c) = (0, 0, 0)
-         */
-        double wt = E1*E1*E1;
-        result.fill(0, 0, 0, wt);
-        if constexpr(doUnmatched){
-            if(!matched1){
-                unmatched_gen->fill(0, 0, 0, wt);
-            }
-        }
-        if constexpr(doTransfer){
-            res3_transferloop(
-                    *transfer,
-                    thisjet_reco,
-                    *axes_reco,
-                    0, 0, 0,
-                    *n1,
-                    *n1,
-                    *n1,
-                    wt);
-        }
-
-
-        for(unsigned i2=i1+1; i2<thisjet_gen->N; ++i2){
-            const auto&[E2, eta2, phi2] = thisjet_gen->singles.get(i2);
-            [[maybe_unused]] bool matched2;
-            if constexpr(doUnmatched){
-                matched2 = matched1 && matched->at(i2);
-            }
-            EEC::neighborhood const * n2 = nullptr;
-            if constexpr(doTransfer){
-                n2 = &(adj->get_neighborhood(i2));
-            }
-
-            const double E12 = E1*E2;
-
-            const auto&[deta12, dphi12, dR12] = thisjet_gen->pairs.get(i1, i2);
-
-            /*
-             * Two-particle part
-             * This has symmetry factor 3!/2! = 3
-             * And RL = R12, r=c=0
-             */
-            unsigned R_idx;
-            if constexpr(PairsType::distances_squared){
-                R_idx = simon::getIndex(std::sqrt(dR12), axes_gen.R);
-            } else {
-                R_idx = simon::getIndex(dR12, axes_gen.R);
-            }
-            wt = 3 * E12 * (E1 + E2);
-            result.fill(R_idx, 0, 0, wt);
-            if constexpr(doUnmatched){
-                if(!matched2){
-                    unmatched_gen->fill(R_idx, 0, 0, wt);
-                }
-            }
-            if constexpr(doTransfer){
-                double wt1 = 3*E12*E1;
-                res3_transferloop(
-                        *transfer,
-                        thisjet_reco,
-                        *axes_reco,
-                        R_idx, 0, 0,
-                        *n1,
-                        *n1,
-                        *n2,
-                        wt1);
-                double wt2 = 3*E12*E2;
-                res3_transferloop(
-                        *transfer,
-                        thisjet_reco,
-                        *axes_reco,
-                        R_idx, 0, 0,
-                        *n1,
-                        *n2,
-                        *n2,
-                        wt2);
-            }
-
-            for(unsigned i3=i2+1; i3<thisjet_gen->N; ++i3){
-                const auto&[E3, eta3, phi3] = thisjet_gen->singles.get(i3);
-                [[maybe_unused]] bool matched3;
-                if constexpr(doUnmatched){
-                    matched3 = matched2 && matched->at(i3);
-                }
-                EEC::neighborhood const * n3 = nullptr;
-                if constexpr(doTransfer){
-                    n3 = &(adj->get_neighborhood(i3));
-                }
-
-                const double E123 = E12*E3;
-
-                const auto&[deta13, dphi13, dR13] = thisjet_gen->pairs.get(i1, i3);
-                const auto&[deta23, dphi23, dR23] = thisjet_gen->pairs.get(i2, i3);
-
-                /*
-                 * Three-particle part
-                 * This has symmetry factor 3! = 6
-                 * and full coordinates
-                 */
-                unsigned r_idx, c_idx;
-                get_indices<PairsType::distances_squared>(dR12, dR13, dR23, R_idx, r_idx, c_idx, axes_gen);
-                
-                wt = E123*6;
-                result.fill(R_idx, r_idx, c_idx, wt);
-                if constexpr(doUnmatched){
-                    if(!matched3){
-                        unmatched_gen->fill(R_idx, r_idx, c_idx, wt);
-                    }
-                }
-                if constexpr(doTransfer){
-                    res3_transferloop(
-                            *transfer,
-                            thisjet_reco,
-                            *axes_reco,
-                            R_idx, r_idx, c_idx,
-                            *n1,
-                            *n2,
-                            *n3,
-                            wt);
-                }
-            }
-        }
-    }
-}
 
 template <class BasicContainer, class PairsType>
 static void call_res3(
@@ -251,7 +25,7 @@ static void call_res3(
             J, nt);
     result.set_pt_denom(thisjet->singles.get_pt_denom());
 
-    res3_mainloop<BasicContainer, PairsType, false, BasicContainer, false>(
+    res3_mainloop<false, BasicContainer, PairsType, false, BasicContainer, false>(
             result,
             nullptr,
             nullptr, 
@@ -318,7 +92,7 @@ static void call_res3_matched(
     result.set_pt_denom(thisjet->singles.get_pt_denom());
     unmatched.set_pt_denom(thisjet->singles.get_pt_denom());
 
-    res3_mainloop<BasicContainer, PairsType, true, BasicContainer, false>(
+    res3_mainloop<false, BasicContainer, PairsType, true, BasicContainer, false>(
             result, 
             &unmatched,
             nullptr, 
@@ -435,7 +209,7 @@ static void call_res3_transfer(
         }
     }
 
-    res3_mainloop<BasicContainer, PairsType, true, TransferContainer, true>(
+    res3_mainloop<false, BasicContainer, PairsType, true, TransferContainer, true>(
             result_gen,
             &unmatched_gen,
             &transfer,
@@ -630,5 +404,3 @@ void EEC::Res3TransferCalculator::compute_precomputed(
         axes_reco,
         axes_gen);
 }
-
-

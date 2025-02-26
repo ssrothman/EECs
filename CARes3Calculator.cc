2b7c96ffe4a25248d96d78fc71a29595fe88c068
Simon Rothman <srothman@wesleyan.edu>
2025-02-26 10:46:21 -0500

    CARes3

---
+++ CARes3Calculator.cc
@@ -0,0 +1,406 @@
+#include "CARes3Calculator.h"
+#include "Adjacency.h"
+#include "Res3Result.h"
+#include "Res3TransferResult.h"
+#include "Res3Backend.h"
+
+#include "SRothman/SimonTools/src/deltaR.h"
+#include "SRothman/SimonTools/src/histutil.h"
+#include "SRothman/SimonTools/src/util.h"
+
+#include <memory>
+
+#include <array>
+
+template <class BasicContainer, class PairsType>
+static void call_res3(
+        EEC::Res3Result<BasicContainer>& result,
+
+        const simon::jet& J,
+        const EEC::normType& nt,
+
+        const EEC::Res3Axes& axes) noexcept {
+
+    auto thisjet = std::make_shared<EEC::EECjet<PairsType>>(
+            J, nt);
+    result.set_pt_denom(thisjet->singles.get_pt_denom());
+
+    res3_mainloop<true, BasicContainer, PairsType, false, BasicContainer, false>(
+            result,
+            nullptr,
+            nullptr, 
+            nullptr,
+            thisjet, 
+            nullptr,
+            nullptr,
+            nullptr,
+            axes);
+}
+
+void EEC::CARes3Calculator::compute_JIT(
+        const simon::jet& J,
+        Res3Result_Vector& result) const noexcept {
+    
+    call_res3<Res3VectorContainer, JITPairs>(
+            result, 
+            J, nt, 
+            axes);
+}
+
+void EEC::CARes3Calculator::compute_JIT(
+        const simon::jet& J,
+        Res3Result_MultiArray& result) const noexcept {
+
+    call_res3<Res3MultiArrayContainer, JITPairs>(
+            result, 
+            J, nt, 
+            axes);
+}
+
+void EEC::CARes3Calculator::compute_precomputed(
+        const simon::jet& J,
+        Res3Result_Vector& result) const noexcept {
+    
+    call_res3<Res3VectorContainer, PrecomputedPairs>(
+            result, 
+            J, nt, 
+            axes);
+}
+
+void EEC::CARes3Calculator::compute_precomputed(
+        const simon::jet& J,
+        Res3Result_MultiArray& result) const noexcept {
+
+    call_res3<Res3MultiArrayContainer, PrecomputedPairs>(
+            result, 
+            J, nt, 
+            axes);
+}
+
+template <class BasicContainer, class PairsType>
+static void call_res3_matched(
+        EEC::Res3Result<BasicContainer>& result,
+        EEC::Res3Result<BasicContainer>& unmatched,
+
+        const simon::jet& J,
+        const std::vector<bool>& matched,
+        const EEC::normType& nt,
+
+        const EEC::Res3Axes& axes) noexcept {
+
+    auto thisjet = std::make_shared<const EEC::EECjet<PairsType>>(J, nt);
+    result.set_pt_denom(thisjet->singles.get_pt_denom());
+    unmatched.set_pt_denom(thisjet->singles.get_pt_denom());
+
+    res3_mainloop<true, BasicContainer, PairsType, true, BasicContainer, false>(
+            result, 
+            &unmatched,
+            nullptr, 
+            nullptr,
+            thisjet,
+            &matched,
+            nullptr,
+            nullptr,
+            axes);
+}
+
+void EEC::CARes3Calculator::compute_JIT_matched(
+        const simon::jet& J,
+        const std::vector<bool>& matched,
+        Res3Result_Vector& result,
+        Res3Result_Vector& unmatched) const noexcept {
+
+    call_res3_matched<Res3VectorContainer, JITPairs>(
+            result, 
+            unmatched,
+            J, matched, nt, 
+            axes);
+}
+
+void EEC::CARes3Calculator::compute_JIT_matched(
+        const simon::jet& J,
+        const std::vector<bool>& matched,
+        Res3Result_MultiArray& result,
+        Res3Result_MultiArray& unmatched) const noexcept {
+
+    call_res3_matched<Res3MultiArrayContainer, JITPairs>(
+            result, 
+            unmatched,
+            J, matched, nt, 
+            axes); 
+}
+
+void EEC::CARes3Calculator::compute_precomputed_matched(
+        const simon::jet& J,
+        const std::vector<bool>& matched,
+        Res3Result_Vector& result,
+        Res3Result_Vector& unmatched) const noexcept {
+
+    call_res3_matched<Res3VectorContainer, PrecomputedPairs>(
+            result, 
+            unmatched,
+            J, matched, nt, 
+            axes);
+}
+
+void EEC::CARes3Calculator::compute_precomputed_matched(
+        const simon::jet& J,
+        const std::vector<bool>& matched,
+        Res3Result_MultiArray& result,
+        Res3Result_MultiArray& unmatched) const noexcept {
+
+    call_res3_matched<Res3MultiArrayContainer, PrecomputedPairs>(
+            result, 
+            unmatched,
+            J, matched, nt, 
+            axes);
+}
+
+template <class PairsType, class TransferContainer, class BasicContainer>
+static void call_res3_transfer(
+        EEC::Res3Result<BasicContainer>& result_gen,
+        EEC::Res3Result<BasicContainer>& unmatched_gen,
+        EEC::Res3TransferResult<TransferContainer>& transfer,
+
+        const simon::jet& J_reco,
+        const simon::jet& J_gen,
+        const EEC::normType& nt,
+
+        const Eigen::MatrixXd& adjmat,
+
+        const EEC::Res3Axes& axes_reco,
+        const EEC::Res3Axes& axes_gen) noexcept {
+
+    auto thisjet_reco = std::make_shared<const EEC::EECjet<PairsType>>(J_reco, nt);
+    auto thisjet_gen = std::make_shared<const EEC::EECjet<PairsType>>(J_gen, nt);
+
+    result_gen.set_pt_denom(thisjet_gen->singles.get_pt_denom());
+    transfer.set_pt_denom(thisjet_reco->singles.get_pt_denom(),
+                          thisjet_gen->singles.get_pt_denom());
+
+    double denom_reco = thisjet_reco->singles.get_pt_denom();
+    double denom_gen = thisjet_gen->singles.get_pt_denom();
+    Eigen::VectorXd ptgen = J_gen.ptvec()/denom_gen;
+    Eigen::VectorXd ptreco = J_reco.ptvec()/denom_reco;
+    Eigen::VectorXd ptfwd = adjmat*ptgen;
+
+    Eigen::MatrixXd adjmat_copy(adjmat);
+    for(unsigned iRecoPart=0; iRecoPart<J_reco.nPart; ++iRecoPart){
+        if(ptfwd(iRecoPart) == 0){
+            continue;
+        }
+        double factor = ptreco(iRecoPart) / ptfwd(iRecoPart);
+        for(unsigned iGenPart=0; iGenPart<J_gen.nPart; ++iGenPart){
+            if(adjmat(iRecoPart, iGenPart) > 0){
+                adjmat_copy(iRecoPart, iGenPart) *= factor;
+            }
+        }
+    }
+
+    auto adj = std::make_shared<const EEC::Adjacency>(adjmat_copy);
+
+    std::vector<bool> matched(J_gen.nPart, false);
+    for(unsigned iGenPart=0; iGenPart<J_gen.nPart; ++iGenPart){
+        for(unsigned iRecoPart=0; iRecoPart<J_reco.nPart; ++iRecoPart){
+            if(adjmat_copy(iRecoPart, iGenPart) > 0){
+                matched[iGenPart] = true;
+                break;
+            }
+        }
+    }
+
+    res3_mainloop<true, BasicContainer, PairsType, true, TransferContainer, true>(
+            result_gen,
+            &unmatched_gen,
+            &transfer,
+            thisjet_reco,
+            thisjet_gen,
+            &matched,
+            adj,
+            &axes_reco,
+            axes_gen);
+}
+
+void EEC::CARes3TransferCalculator::compute_JIT(
+        const simon::jet& J_reco,
+        const simon::jet& J_gen, 
+        const Eigen::MatrixXd& tmat,
+        Res3Result_Vector& result,
+        EEC::Res3Result_Vector& unmatched_gen,
+        Res3TransferResult_Vector& tresult) const noexcept {
+
+    call_res3_transfer<JITPairs>(
+        result,
+        unmatched_gen,
+        tresult,
+
+        J_reco,
+        J_gen,
+        nt,
+
+        tmat,
+
+        axes_reco,
+        axes_gen);
+}
+
+void EEC::CARes3TransferCalculator::compute_JIT(
+        const simon::jet& J_reco,
+        const simon::jet& J_gen, 
+        const Eigen::MatrixXd& tmat,
+        Res3Result_MultiArray& result,
+        EEC::Res3Result_MultiArray& unmatched_gen,
+        Res3TransferResult_Vector& tresult) const noexcept {
+
+    call_res3_transfer<JITPairs>(
+        result,
+        unmatched_gen,
+        tresult,
+
+        J_reco,
+        J_gen,
+        nt,
+
+        tmat,
+
+        axes_reco,
+        axes_gen);
+}
+
+void EEC::CARes3TransferCalculator::compute_JIT(
+        const simon::jet& J_reco,
+        const simon::jet& J_gen, 
+        const Eigen::MatrixXd& tmat,
+        Res3Result_Vector& result,
+        EEC::Res3Result_Vector& unmatched_gen,
+        Res3TransferResult_MultiArray& tresult) const noexcept {
+
+    call_res3_transfer<JITPairs>(
+        result,
+        unmatched_gen,
+        tresult,
+
+        J_reco,
+        J_gen,
+        nt,
+
+        tmat,
+
+        axes_reco,
+        axes_gen);
+}
+
+void EEC::CARes3TransferCalculator::compute_JIT(
+        const simon::jet& J_reco,
+        const simon::jet& J_gen, 
+        const Eigen::MatrixXd& tmat,
+        Res3Result_MultiArray& result,
+        EEC::Res3Result_MultiArray& unmatched_gen,
+        Res3TransferResult_MultiArray& tresult) const noexcept {
+
+    call_res3_transfer<JITPairs>(
+        result,
+        unmatched_gen,
+        tresult,
+
+        J_reco,
+        J_gen,
+        nt,
+
+        tmat,
+
+        axes_reco,
+        axes_gen);
+}
+
+void EEC::CARes3TransferCalculator::compute_precomputed(
+        const simon::jet& J_reco,
+        const simon::jet& J_gen, 
+        const Eigen::MatrixXd& tmat,
+        Res3Result_Vector& result,
+        EEC::Res3Result_Vector& unmatched_gen,
+        Res3TransferResult_Vector& tresult) const noexcept {
+
+    call_res3_transfer<PrecomputedPairs>(
+        result,
+        unmatched_gen,
+        tresult,
+
+        J_reco,
+        J_gen,
+        nt,
+
+        tmat,
+
+        axes_reco,
+        axes_gen);
+}
+
+void EEC::CARes3TransferCalculator::compute_precomputed(
+        const simon::jet& J_reco,
+        const simon::jet& J_gen, 
+        const Eigen::MatrixXd& tmat,
+        Res3Result_MultiArray& result,
+        EEC::Res3Result_MultiArray& unmatched_gen,
+        Res3TransferResult_Vector& tresult) const noexcept {
+
+    call_res3_transfer<PrecomputedPairs>(
+        result,
+        unmatched_gen,
+        tresult,
+
+        J_reco,
+        J_gen,
+        nt,
+
+        tmat,
+
+        axes_reco,
+        axes_gen);
+}
+
+void EEC::CARes3TransferCalculator::compute_precomputed(
+        const simon::jet& J_reco,
+        const simon::jet& J_gen, 
+        const Eigen::MatrixXd& tmat,
+        Res3Result_Vector& result,
+        EEC::Res3Result_Vector& unmatched_gen,
+        Res3TransferResult_MultiArray& tresult) const noexcept {
+
+    call_res3_transfer<PrecomputedPairs>(
+        result,
+        unmatched_gen,
+        tresult,
+
+        J_reco,
+        J_gen,
+        nt,
+
+        tmat,
+
+        axes_reco,
+        axes_gen);
+}
+
+void EEC::CARes3TransferCalculator::compute_precomputed(
+        const simon::jet& J_reco,
+        const simon::jet& J_gen, 
+        const Eigen::MatrixXd& tmat,
+        Res3Result_MultiArray& result,
+        EEC::Res3Result_MultiArray& unmatched_gen,
+        Res3TransferResult_MultiArray& tresult) const noexcept {
+
+    call_res3_transfer<PrecomputedPairs>(
+        result,
+        unmatched_gen,
+        tresult,
+
+        J_reco,
+        J_gen,
+        nt,
+
+        tmat,
+
+        axes_reco,
+        axes_gen);
+}
---
+++ CARes3Calculator.h
@@ -0,0 +1,176 @@
+#ifndef SROTHMAN_EECS_V2_CARES3CALCULATOR_H
+#define SROTHMAN_EECS_V2_CARES3CALCULATOR_H
+
+#include "Res3Axes.h"
+#include "EECjet.h"
+
+#include "Res3Result_forward.h"
+#include "Res3TransferResult_forward.h"
+
+#ifdef CMSSW_GIT_HASH
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+#endif
+
+namespace EEC{
+    class CARes3Calculator {
+    public:
+        CARes3Calculator(
+                const std::vector<double>& R,
+                const std::vector<double>& r,
+                const std::vector<double>& c,
+                const normType nt) :
+            axes(R, r, c),
+            nt(nt) {}
+
+#ifdef CMSSW_GIT_HASH
+        CARes3Calculator(const edm::ParameterSet& iConfig);
+        static void fillPSetDescription(edm::ParameterSetDescription& desc);
+#endif
+
+        void compute_JIT(
+                const simon::jet& J, 
+                Res3Result_Vector& result) const noexcept;
+
+        void compute_JIT(
+                const simon::jet& J, 
+                Res3Result_MultiArray& result) const noexcept;
+
+        void compute_precomputed(
+                const simon::jet& J, 
+                Res3Result_Vector& result) const noexcept;
+
+        void compute_precomputed(
+                const simon::jet& J, 
+                Res3Result_MultiArray& result) const noexcept;
+
+        void compute_JIT_matched(
+                const simon::jet& J,
+                const std::vector<bool>& matched,
+                Res3Result_Vector& result,
+                Res3Result_Vector& unmatched) const noexcept;
+
+        void compute_JIT_matched(
+                const simon::jet& J,
+                const std::vector<bool>& matched,
+                Res3Result_MultiArray& result,
+                Res3Result_MultiArray& unmatched) const noexcept;
+
+        void compute_precomputed_matched(
+                const simon::jet& J,
+                const std::vector<bool>& matched,
+                Res3Result_Vector& result,
+                Res3Result_Vector& unmatched) const noexcept;
+
+        void compute_precomputed_matched(
+                const simon::jet& J,
+                const std::vector<bool>& matched,
+                Res3Result_MultiArray& result,
+                Res3Result_MultiArray& unmatched) const noexcept;
+
+        const Res3Axes& get_axes() const noexcept{
+            return axes;
+        }
+    private:
+        const Res3Axes axes;
+        const EEC::normType nt;
+    };
+
+    class CARes3TransferCalculator {
+    public:
+        CARes3TransferCalculator(
+                const std::vector<double>& R_reco,
+                const std::vector<double>& r_reco,
+                const std::vector<double>& c_reco,
+                const std::vector<double>& R_gen,
+                const std::vector<double>& r_gen,
+                const std::vector<double>& c_gen,
+                const normType nt) :
+            axes_reco(R_reco, r_reco, c_reco),
+            axes_gen(R_gen, r_gen, c_gen),
+            nt(nt) {}
+
+#ifdef CMSSW_GIT_HASH
+        CARes3TransferCalculator(const edm::ParameterSet& iConfig);
+        static void fillPSetDescription(edm::ParameterSetDescription& desc);
+#endif
+
+        void compute_JIT(
+                const simon::jet& J_reco,
+                const simon::jet& J_gen, 
+                const Eigen::MatrixXd& tmat,
+                Res3Result_Vector& result,
+                EEC::Res3Result_Vector& unmatched_gen,
+                Res3TransferResult_Vector& tresult) const noexcept;
+
+        void compute_JIT(
+                const simon::jet& J_reco,
+                const simon::jet& J_gen, 
+                const Eigen::MatrixXd& tmat,
+                Res3Result_MultiArray& result,
+                EEC::Res3Result_MultiArray& unmatched_gen,
+                Res3TransferResult_Vector& tresult) const noexcept;
+
+        void compute_JIT(
+                const simon::jet& J_reco,
+                const simon::jet& J_gen, 
+                const Eigen::MatrixXd& tmat,
+                Res3Result_Vector& result,
+                EEC::Res3Result_Vector& unmatched_gen,
+                Res3TransferResult_MultiArray& tresult) const noexcept;
+
+        void compute_JIT(
+                const simon::jet& J_reco,
+                const simon::jet& J_gen, 
+                const Eigen::MatrixXd& tmat,
+                Res3Result_MultiArray& result,
+                EEC::Res3Result_MultiArray& unmatched_gen,
+                Res3TransferResult_MultiArray& tresult) const noexcept;
+
+        void compute_precomputed(
+                const simon::jet& J_reco,
+                const simon::jet& J_gen, 
+                const Eigen::MatrixXd& tmat,
+                Res3Result_Vector& result,
+                EEC::Res3Result_Vector& unmatched_gen,
+                Res3TransferResult_Vector& tresult) const noexcept;
+
+        void compute_precomputed(
+                const simon::jet& J_reco,
+                const simon::jet& J_gen, 
+                const Eigen::MatrixXd& tmat,
+                Res3Result_MultiArray& result,
+                EEC::Res3Result_MultiArray& unmatched_gen,
+                Res3TransferResult_Vector& tresult) const noexcept;
+
+        void compute_precomputed(
+                const simon::jet& J_reco,
+                const simon::jet& J_gen, 
+                const Eigen::MatrixXd& tmat,
+                Res3Result_Vector& result,
+                EEC::Res3Result_Vector& unmatched_gen,
+                Res3TransferResult_MultiArray& tresult) const noexcept;
+
+        void compute_precomputed(
+                const simon::jet& J_reco,
+                const simon::jet& J_gen, 
+                const Eigen::MatrixXd& tmat,
+                Res3Result_MultiArray& result,
+                EEC::Res3Result_MultiArray& unmatched_gen,
+                Res3TransferResult_MultiArray& tresult) const noexcept;
+
+        const Res3Axes& get_axes_reco() const noexcept{
+            return axes_reco;
+        }
+
+        const Res3Axes& get_axes_gen() const noexcept{
+            return axes_gen;
+        }
+
+    private:
+        const Res3Axes axes_reco, axes_gen;
+        const EEC::normType nt;
+    };
+};
+
+#endif
